ae99d3777d4e708a74657fe4425c17c80e3486a0
Jeremy Kahn <jeremyckahn@Jeremy-Kahns-iMac.local>
2010-10-07 07:31:58 -0500

    Got a basic triangle representing the player to draw to the screen.

--- testing/js/draw/entity.js
+++ testing/js/draw/entity.js
@@ -1,19 +1,23 @@
 function entity(game, params){
 	this.init(game, params);
+	this.state = this.params.state || {};
 
 	this.render = function(){
-		/*if (this.params.gameShape && this.game.shapes[this.params.gameShape]){
-			this.game.shapes[this.params.gameShape](this.params);
-		} else {
-			log('No shape provided for entity ' + this);
-		}*/
 		
 		if (this.params.gameShape){
-			this.params.gameShape(this.params);
+			this.params.gameShape(this, this.params, this.state);
 		} else {
 			log('No shape provided for entity ' + this);
 		}
 	}
+	
+	this.setState = function(state){
+		this.state = state;
+	};
+	
+	this.updateState = function(state){
+		$.extend(true, this.state, state);
+	};
 }
 
 entity.prototype = drawable;
